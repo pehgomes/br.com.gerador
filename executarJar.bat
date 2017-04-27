98dd571f120fc134ce33a4fdb47aa91676e0dcd2
Pedro H. Gomes <pedrogomesup2@gmail.com>
2017-04-27 18:00:05 -0300

    Add files via upload

---
+++ executarJar.bat
@@ -0,0 +1,49 @@
+@echo off
+color 0a
+
+@echo Ativando bot .
+ping -n 2 -w 1000 0.0.0.1 > nul
+cls
+@echo Ativando bot ..
+ping -n 2 -w 1000 0.0.0.1 > nul
+cls
+@echo Ativando bot ...
+ping -n 2 -w 1000 0.0.0.1 > nul
+cls
+
+@echo Ativando bot .
+ping -n 2 -w 1000 0.0.0.1 > nul
+cls
+@echo Ativando bot ..
+ping -n 2 -w 1000 0.0.0.1 > nul
+cls
+@echo Ativando bot ...
+ping -n 2 -w 1000 0.0.0.1 > nul
+
+
+
+@echo preparando threads .
+ping -n 2 -w 1000 0.0.0.1 > nul
+cls
+@echo preparando threads ..
+ping -n 2 -w 1000 0.0.0.1 > nul
+cls
+@echo preparando threads ...
+ping -n 2 -w 1000 0.0.0.1 > nul
+cls
+@echo preparando threads .
+ping -n 2 -w 1000 0.0.0.1 > nul
+cls
+@echo preparando threads ..
+ping -n 2 -w 1000 0.0.0.1 > nul
+cls
+@echo preparando threads ...
+ping -n 2 -w 1000 0.0.0.1 > nul
+cls
+
+
+cd\
+cd C:\ 
+java -jar gerador.jar
+
+
